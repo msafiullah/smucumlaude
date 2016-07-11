644913a143e52013e43d9d8306526ba9aab56f8e
msafiullah (Mac) <msafiullah@gmail.com>
2016-07-11 20:39:03 +0800

    Fix bug.

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
+var dynatable = null;
+var dataMods = null;
+
 $.ajax({
   url: 'modssmu.min.json',
   success: function(data) {
-
-    $('#mods-table').dynatable({
-      dataset: {
-        records: data.records
-      },
-      table: {
-        defaultColumnIdStyle: 'trimDash'
-      },
-      features: {
-        search: false
-      }
-    });
-
+    dataMods = data.records;
   }
 });
 
@@ -40,46 +31,48 @@ function filterData(name, record, value) {
   }
 }
 
-var dynatable = $('#mods-table')
-  .bind('dynatable:init', function(e, dynatable) {
-    dynatable.queries.functions['filter-1'] = function(record, queryValue) {
-      return filterData('A', record, queryValue);
-    };
-    dynatable.queries.functions['filter-2'] = function(record, queryValue) {
-      return filterData('A-', record, queryValue);
-    };
-    dynatable.queries.functions['filter-3'] = function(record, queryValue) {
-      return filterData('B+', record, queryValue);
-    };
-    dynatable.queries.functions['filter-4'] = function(record, queryValue) {
-      return filterData('B', record, queryValue);
-    };
-    dynatable.queries.functions['filter-5'] = function(record, queryValue) {
-      return filterData('B-', record, queryValue);
-    };
-    dynatable.queries.functions['filter-6'] = function(record, queryValue) {
-      return filterData('C+', record, queryValue);
-    };
-    dynatable.queries.functions['filter-7'] = function(record, queryValue) {
-      return filterData('C', record, queryValue);
-    };
-    dynatable.queries.functions['filter-8'] = function(record, queryValue) {
-      return filterData('C-', record, queryValue);
-    };
-    dynatable.queries.functions['filter-9'] = function(record, queryValue) {
-      return filterData('CGPA', record, queryValue);
-    };
-  })
-  .dynatable({
-    dataset: dataMods,
-    table: {
-      defaultColumnIdStyle: 'trimDash'
-    },
-    features: {
-      search: false,
-      pushState: false
-    },
-    inputs: {
-      queries: $('#filter-1, #filter-2, #filter-3, #filter-4, #filter-5, #filter-6, #filter-7, #filter-8, #filter-9')
-    }
-  }).data('dynatable');
+function initDynatable() {
+  dynatable = $('#mods-table')
+    .bind('dynatable:init', function(e, dynatable) {
+      dynatable.queries.functions['filter-1'] = function(record, queryValue) {
+        return filterData('A', record, queryValue);
+      };
+      dynatable.queries.functions['filter-2'] = function(record, queryValue) {
+        return filterData('A-', record, queryValue);
+      };
+      dynatable.queries.functions['filter-3'] = function(record, queryValue) {
+        return filterData('B+', record, queryValue);
+      };
+      dynatable.queries.functions['filter-4'] = function(record, queryValue) {
+        return filterData('B', record, queryValue);
+      };
+      dynatable.queries.functions['filter-5'] = function(record, queryValue) {
+        return filterData('B-', record, queryValue);
+      };
+      dynatable.queries.functions['filter-6'] = function(record, queryValue) {
+        return filterData('C+', record, queryValue);
+      };
+      dynatable.queries.functions['filter-7'] = function(record, queryValue) {
+        return filterData('C', record, queryValue);
+      };
+      dynatable.queries.functions['filter-8'] = function(record, queryValue) {
+        return filterData('C-', record, queryValue);
+      };
+      dynatable.queries.functions['filter-9'] = function(record, queryValue) {
+        return filterData('CGPA', record, queryValue);
+      };
+    })
+    .dynatable({
+      dataset: dataMods,
+      table: {
+        defaultColumnIdStyle: 'trimDash'
+      },
+      features: {
+        search: false,
+        pushState: false
+      },
+      inputs: {
+        queries: $('#filter-1, #filter-2, #filter-3, #filter-4, #filter-5, #filter-6, #filter-7, #filter-8, #filter-9')
+      }
+    }).data('dynatable');
+}
